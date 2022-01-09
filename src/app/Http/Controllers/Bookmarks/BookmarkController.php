50b5a33d768431dfa93e3844dceea9438aa12a5a
gutimitsu <gutimitsuguti@gmail.com>
2022-01-09 23:48:54 +0900

    4-6 実際に外部アクセスはしない「モック」を作成しよう

--- src/app/Http/Controllers/Bookmarks/BookmarkController.php
+++ src/app/Http/Controllers/Bookmarks/BookmarkController.php
@@ -7,6 +7,7 @@ use App\Bookmark\UseCase\CreateBookmarkUseCase;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\CreateBookmarkRequest;
 use App\Lib\LinkPreview\LinkPreview;
+use App\Lib\LinkPreview\MockLinkPreview;
 use App\Models\Bookmark;
 use App\Models\BookmarkCategory;
 use App\Models\User;
@@ -127,7 +128,7 @@ class BookmarkController extends Controller
      */
     public function create(CreateBookmarkRequest $request)
     {
-        $useCase = new CreateBookmarkUseCase(new LinkPreview());
+        $useCase = new CreateBookmarkUseCase(new MockLinkPreview());
         $useCase->handle($request->url, $request->category, $request->comment);
         // 暫定的に成功時は一覧ページへ
         return redirect('/bookmarks', 302);
